94d9ce32be519bd04b1c84af397637615465ed11
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-05-18 23:33:31 +0300

    CollectArticleTask is added

--- test/hw_001_data_parsing/parser/parser_test.py
+++ test/hw_001_data_parsing/parser/parser_test.py
@@ -1,7 +1,8 @@
 import unittest
 from bs4 import BeautifulSoup
 
-from src.hw_001_data_parsing.parser.parser import Parser, CollectPageLinksTask, CollectPublishedDatetime, CollectViewCounter
+from src.hw_001_data_parsing.parser.parser import Parser, CollectPageLinksTask, CollectPublishedDatetime, \
+    CollectViewCounter, CollectArticleTask
 
 
 class TestCase(unittest.TestCase):
@@ -48,10 +49,10 @@ class TestCase(unittest.TestCase):
         self.assertEqual(expected_soups, task._soups)
 
     def test_collect_page_link_task(self):
-        expected_links = ["/ru/articles/734928/",
-                          "/ru/articles/734929/",
-                          "/ru/articles/734930/",
-                          "/ru/articles/734932/"]
+        expected_links = ["https://habr.com/ru/articles/734928/",
+                          "https://habr.com/ru/articles/734929/",
+                          "https://habr.com/ru/articles/734930/",
+                          "https://habr.com/ru/articles/734932/"]
         page0 = """
         <div>
             <a
@@ -102,8 +103,8 @@ class TestCase(unittest.TestCase):
         </div>
         """
 
-        task = CollectPageLinksTask('a', 'tm-title__link')\
-            .execute('', BeautifulSoup(page0, 'html.parser'))\
+        task = CollectPageLinksTask('a', 'tm-title__link') \
+            .execute('', BeautifulSoup(page0, 'html.parser')) \
             .execute('', BeautifulSoup(page1, 'html.parser'))
 
         self.assertEqual(expected_links, task.attrs[task.KEY])
@@ -142,6 +143,42 @@ class TestCase(unittest.TestCase):
 
         self.assertEqual(True, expected_key in task.attrs)
         self.assertEqual(expected_count, task.attrs[expected_key])
+
+    def test_collect_article_task(self):
+        expected_key = 'some.key'
+        words = [f'word{w}' for w in range(0, 14)]
+        page = f"""
+        <div>
+            <div class="article-formatted-body article-formatted-body article-formatted-body_version-2">
+                <p>{words[0]}</p>
+                <p>{words[1]}</p>
+                <a href="https://habr.com/ru/users/NewTechAudit/posts/" rel="noopener noreferrer nofollow">{words[2]}</a>
+                <figure class="full-width "><img src="https://habrastorage.org/getpro/habr/upload_files/f5a/0bc/288/f5a0bc2880d7bfb2889ca5a9ade49dc5.jpg" width="1280" height="720"><figcaption></figcaption></figure>
+                <pre>
+                    <code>
+                        {{{words[3]}: {words[4]}, {words[5]}: ‘{words[6]}’, {words[7]}: ‘{words[8]}’, {words[9]}: ‘{words[10]}’}}
+                    </code>
+                </pre>
+                <ul>
+                    <li><p>{words[11]}</p></li>
+                    <li><p>{words[12]}</p></li>
+                    <li><p>{words[13]}</p></li>
+                </ul>
+            </div>
+        </div>
+                """
+
+        soup = BeautifulSoup(page, 'html.parser')
+        task = CollectArticleTask('div', 'article-formatted-body')
+        task.execute(expected_key, soup)
+
+        expected_text = ''
+        delimiter = ''
+        for w in words:
+            expected_text += delimiter + w
+            delimiter = ' '
+        self.assertEqual(True, expected_key in task.attrs)
+        self.assertEqual(expected_text, task.attrs[expected_key])
 
 
 if __name__ == '__main__':
