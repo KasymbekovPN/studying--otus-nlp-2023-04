5f5f117e061f83dc9c9a63afe95f09887f76b5d2
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-05-27 14:35:02 +0300

    base_generator is added

---
+++ src/hw_002_py_tourch/source/args/generator/base_generator.py
@@ -0,0 +1,14 @@
+
+class BaseGenerator:
+    TYPE_SETTER_EXC_MSG = 'Type setter is unsupported'
+
+    def __init__(self, type_):
+        self._type = type_
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, value):
+        raise Exception(self.TYPE_SETTER_EXC_MSG)
---
+++ test/hw_002_py_tourch/source/args/generator/base_generator_test.py
@@ -0,0 +1,28 @@
+import unittest
+
+from parameterized import parameterized
+from src.hw_002_py_tourch.source.args.generator.base_generator import BaseGenerator
+
+
+class TestCase(unittest.TestCase):
+    @parameterized.expand([
+        (float, float),
+        (str, str),
+        (int, int),
+        (1, 1),
+        (1.0, 1.0),
+        ('some.type', 'some.type')
+    ])
+    def test_type_getting(self, init_type, expected_type):
+        base_generator = BaseGenerator(init_type)
+        self.assertEqual(expected_type, base_generator.type)
+
+    def test_type_setting(self):
+        base_generator = BaseGenerator(float)
+        with self.assertRaises(Exception) as context:
+            base_generator.type = 123
+        self.assertTrue(base_generator.TYPE_SETTER_EXC_MSG in context.exception.args)
+
+
+if __name__ == '__main__':
+    unittest.main()
