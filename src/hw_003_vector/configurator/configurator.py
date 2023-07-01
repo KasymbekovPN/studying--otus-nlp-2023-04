69a8f887c533550ede3147349edd608e4248214f
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-07-01 18:40:38 +0300

    Grid search is added

--- src/hw_003_vector/configurator/configurator.py
+++ src/hw_003_vector/configurator/configurator.py
@@ -7,7 +7,38 @@ class Configurator(BaseConfigurator):
             'path.dataset.original': '../../datasets/IMDB Dataset.csv',
             'path.dataset.prepared': '../../datasets/prepared IMDB Dataset.csv',  # filtered & lemmatized dataset
 
-            # todo del
-            # 'test_part_percent': 0.3
+            'train-test-split.test-size': 0.2,
+            'train-test-split.random-stage': 42,
+
+            'vec.tfidf.max-features': 200,
+            'vec.tfidf.norm': None,
+            'vec.tfidf.max-df': 0.95,
+            'vec.tfidf.min-df': 5,
+            'vec.tfidf.stop-words': 'english',
+
+            'hyper-params.on': True,
+            'hyper-params.cv': 3,
+            'hyper-params.scoring': 'accuracy',
+            'hyper-params.verbose': 3,
+            'hyper-params.jobs': -1,
+            'hyper-params.param-grid': {
+                'max_depth': [3, None],
+                'n_estimators': [10, 100],
+            },
+            # todo !!! add comment
+            # 'hyper-params.param-grid': {
+            #     'max_depth': [3, 5, 10, None],
+            #     'n_estimators': [10, 100, 200],
+            #     'max_features': [1, 5, 10],
+            #     'min_samples_leaf': [1, 2, 3],
+            #     'min_samples_split': [1, 2, 3]
+            # },
+
+            # todo !!! change values cls.<...>
+            'cls.max-depth': None,
+            'cls.estimators': 500,
+            'cls.max-features': 10,
+            'cls.min-samples-leaf': 2,
+            'cls.min-samples-split': 1
         }
         super().__init__(params)
--- src/hw_003_vector/main.py
+++ src/hw_003_vector/main.py
@@ -1,5 +1,11 @@
 import copy
 import csv
+import datetime
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import *
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import *
 
 from src.hw_003_vector.configurator.configurator import Configurator
 from src.hw_003_vector.reader.dataset_reader import DatasetReader
@@ -9,6 +15,8 @@ from src.hw_003_vector.preparation.regexp_preparators import PunctuationPreparat
 from src.hw_003_vector.preparation.text_lemm_preparator import TextLemmPreparator
 from src.hw_003_vector.preparation.sentiment_read_preparator import SentimentReadPreparator
 from src.hw_003_vector.preparation.sentiment_write_preparator import SentimentWritePreparator
+from src.hw_003_vector.dataset.sentiment_strategies import convert_sentiment_to_str, convert_sentiment_to_int
+from src.hw_003_vector.dataset.dataset import Dataset
 
 
 def write_prepared_dataset(dataset: list, conf: Configurator):
@@ -19,7 +27,7 @@ def write_prepared_dataset(dataset: list, conf: Configurator):
         [writer.writerow(datum) for datum in dataset]
 
 
-def get_dataset(conf: Configurator) -> list[dict] | None:
+def get_dataset(conf: Configurator) -> Dataset | None:
     dataset_reader = DatasetReader()
     dataset_reader(path_original=conf('path.dataset.original'), path_prepared=conf('path.dataset.prepared'))
 
@@ -44,13 +52,22 @@ def get_dataset(conf: Configurator) -> list[dict] | None:
 
     result = [read_preparator(datum) for datum in result]
     if saver is not None:
-        print(6)
         prepared = copy.deepcopy(result)
         write_preparator = Preparation(SentimentWritePreparator())
         prepared = [write_preparator(datum) for datum in prepared]
         saver(prepared, conf)
 
-    return result
+    return Dataset(result,
+                   {'review', 'sentiment'},
+                   strategy_int_sentiment=convert_sentiment_to_int,
+                   strategy_str_sentiment=convert_sentiment_to_str)
+
+
+def print_timediff(tag, prev_time):
+    current = datetime.datetime.now()
+    print(f'[{tag}]: {current - prev_time}')
+
+    return current
 
 
 def run():
@@ -59,6 +76,59 @@ def run():
     dataset = get_dataset(conf)
     if dataset is None:
         return
+
+    current_time = datetime.datetime.now()
+    train_x, test_x, train_y, test_y = train_test_split(
+        dataset.get_column('review'),
+        dataset.get_column('sentiment', 'int_sentiment'),
+        test_size=conf('train-test-split.test-size'),
+        random_state=conf('train-test-split.random-stage'),
+        stratify=dataset.get_column('sentiment', 'str_sentiment')
+    )
+    current_time = print_timediff('slice', current_time)
+
+    vectorizer = TfidfVectorizer(
+        max_features=conf('vec.tfidf.max-features'),
+        norm=conf('vec.tfidf.norm'),
+        max_df=conf('vec.tfidf.max-df'),
+        min_df=conf('vec.tfidf.min-df'),
+        stop_words=conf('vec.tfidf.stop-words')
+    )
+    train_X = vectorizer.fit_transform(train_x)
+    test_X = vectorizer.transform(test_x)
+    current_time = print_timediff('vectorizering', current_time)
+
+    if conf('hyper-params.on'):
+        print('Hyperparams search started')
+        clf = RandomForestClassifier()
+        grid = GridSearchCV(
+            clf,
+            param_grid=conf('hyper-params.param-grid'),
+            cv=conf('hyper-params.cv'),
+            scoring=conf('hyper-params.scoring'),
+            verbose=conf('hyper-params.verbose'),
+            n_jobs=conf('hyper-params.jobs')
+        )
+        model_grid = grid.fit(train_X, train_y)
+        print(f'Best hyperparameters are: {model_grid.best_params_}')
+        print(f'Best score is: {model_grid.best_score_}')
+    else:
+        print('Usage of config params for RandomForestClassifier')
+        clf = RandomForestClassifier(
+            n_estimators=conf('cls.estimators'),
+            max_depth=conf('cls.max-depth'),
+            max_features=conf('cls.max-features'),
+            min_samples_split=conf('cls.min-samples-leaf'),
+            min_samples_leaf=conf('cls.min-samples-split'),
+        )
+
+    clf.fit(train_X, train_y)
+    prediction = clf.predict(test_X)
+    current_time = print_timediff('class', current_time)
+
+    print(f'Accuracy: {accuracy_score(test_y, prediction)}')
+    print(f'F1: {f1_score(test_y, prediction, average="macro")}')
+    _ = print_timediff('metrics', current_time)
 
 
 if __name__ == '__main__':
